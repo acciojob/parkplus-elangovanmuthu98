94aa474efbf2b5113a5d7f729a6f882f64daae87
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2023-12-30 10:29:56 +0530

    Update PaymentController.java

--- src/main/java/com/driver/controllers/PaymentController.java
+++ src/main/java/com/driver/controllers/PaymentController.java
@@ -1,7 +1,35 @@
+// package com.driver.controllers;
+
+// import com.driver.services.impl.PaymentServiceImpl;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
+
+
+// @RestController
+// @RequestMapping(value = "/payment")
+// public class PaymentController {
+	
+// 	@Autowired
+//     PaymentServiceImpl paymentService;
+
+//     @PostMapping("/pay")
+//     public Payment pay(@RequestParam Integer reservationId, @RequestParam Integer amountSent, @RequestParam String mode) throws Exception{
+//         //Attempt a payment of amountSent for reservationId using the given mode ("cASh", "card", or "upi")
+//         //If the amountSent is less than bill, throw "Insufficient Amount" exception, otherwise update payment attributes
+//         //If the mode contains a string other than "cash", "card", or "upi" (any character in uppercase or lowercase), throw "Payment mode not detected" exception.
+//         //Note that the reservationId always exists
+//         return null
+//     }
+// }
+
+
 package com.driver.controllers;
 
+import com.driver.model.Payment;
 import com.driver.services.impl.PaymentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,6 +46,14 @@ public class PaymentController {
         //If the amountSent is less than bill, throw "Insufficient Amount" exception, otherwise update payment attributes
         //If the mode contains a string other than "cash", "card", or "upi" (any character in uppercase or lowercase), throw "Payment mode not detected" exception.
         //Note that the reservationId always exists
-        return null
+        Payment payment;
+        try{
+            payment = paymentService.pay(reservationId, amountSent, mode);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.getMessage());
+        }
+        return payment;
     }
 }
